85eea7d92ad856baa4a90f711a68f878a8c82926
Kellyy621 <yekelly621@gmail.com>
2025-04-02 10:14:51 -0400

    Update flashcard.py

--- flashcard.py
+++ flashcard.py
@@ -10,3 +10,22 @@ def load_flashcards():
 def save_flashcards(flashcards):
      with open ('Flashcards', 'y') as file:
          json.dump(flashcards, file)         
+
+def teacher_mode():
+    flashcards = load_flashcards()
+    while True:
+        word =  input ("Enter word/phrase(or'exit' to stop):")
+        if word.lower() == 'exit':
+                break 
+        answer = input("Enter the answer for '{word}':")
+        flashcards['word']= answer 
+    save_flashcards(flashcards)
+    print ("Your flashcards are saved!")
+
+def student_mode():
+     flashcards = load_flashcards()
+     if not flashcards:
+          print ("TYhere are no flashcards available :(")
+          return
+     score = 0
+        
