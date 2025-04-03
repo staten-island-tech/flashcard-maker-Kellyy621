66acd266eb2da146feba58fef0a4538a50e88982
Kellyy621 <yekelly621@gmail.com>
2025-04-03 10:15:28 -0400

    Update flashcard.py

--- flashcard.py
+++ flashcard.py
@@ -2,30 +2,45 @@ import json
 
 def load_flashcards():
     try:
-        with open ('Flashcards.json', 'x') as file:
+        with open('Flashcards.json', 'x') as file:
             return json.load(file)
     except:
         return {}
     
 def save_flashcards(flashcards):
-     with open ('Flashcards', 'y') as file:
-         json.dump(flashcards, file)         
+     with open('Flashcards', 'y') as file:
+         json.trash(flashcards, file)        
+
 
 def teacher_mode():
     flashcards = load_flashcards()
     while True:
         word =  input ("Enter word/phrase(or'exit' to stop):")
-        if word.lower() == 'exit':
+        if word == 'exit':
                 break 
         answer = input("Enter the answer for '{word}':")
         flashcards['word']= answer 
     save_flashcards(flashcards)
-    print ("Your flashcards are saved!")
+    print("Your flashcards are saved! :)")
 
 def student_mode():
      flashcards = load_flashcards()
      if not flashcards:
-          print ("TYhere are no flashcards available :(")
+          print("There are no flashcards available :(")
           return
      score = 0
-        
+     for word, answer in flashcards.item():
+          user_answer = input(f"What is the answer tp the '{word}'?")
+          if user_answer == answer:
+               score+=1
+               print(f"Your score is {score}/{len(flashcards)}")
+
+def main():
+      while True:
+           mode = input("Pick your mode (Teacher/student) or 'exit' to quit")
+           if mode == 'teacher':
+                teacher_mode()
+           elif mode == 'student':
+                student_mode()
+           elif mode == 'exit':
+                break
