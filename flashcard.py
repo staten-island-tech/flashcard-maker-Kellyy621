ce35c0ff7c8ae94329927d522c39bbcc94a83dd7
Kellyy621 <yekelly621@gmail.com>
2025-04-04 10:17:11 -0400

    yes

--- flashcard.py
+++ flashcard.py
@@ -2,38 +2,14 @@ import json
 
 def load_flashcards():
     try:
-        with open('Flashcards.json', 'x') as file:
+        with open('Flashcards.json', 'r') as file:
             return json.load(file)
-    except:
+    except FileNotFoundError:
         return {}
     
 def save_flashcards(flashcards):
-     with open('Flashcards', 'y') as file:
-         json.trash(flashcards, file)        
-
-
-def teacher_mode():
-    flashcards = load_flashcards()
-    while True:
-        word =  input ("Enter word/phrase(or'exit' to stop):")
-        if word == 'exit':
-                break 
-        answer = input("Enter the answer for '{word}':")
-        flashcards['word']= answer 
-    save_flashcards(flashcards)
-    print("Your flashcards are saved! :)")
-
-def student_mode():
-     flashcards = load_flashcards()
-     if not flashcards:
-          print("There are no flashcards available :(")
-          return
-     score = 0
-     for word, answer in flashcards.item():
-          user_answer = input(f"What is the answer tp the '{word}'?")
-          if user_answer == answer:
-               score+=1
-               print(f"Your score is {score}/{len(flashcards)}")
+     with open('Flashcards', 'w') as file:
+         json.dump(flashcards, file)        
 
 def main():
       while True:
@@ -44,3 +20,36 @@ def main():
                 student_mode()
            elif mode == 'exit':
                 break
+
+def teacher_mode():
+    flashcards = load_flashcards()
+    while True:
+        word =  input ("Enter word/phrase(or'exit' to stop):")
+        if word == 'exit':
+                break 
+        answer = input("Enter the answer for '{word/phrase}':")
+        flashcards[word]= answer 
+    save_flashcards(flashcards)
+    print("Your flashcards are saved! :)")
+
+def student_mode():
+     flashcards = load_flashcards()
+     print("Loaded flashcards in student mode:", flashcards)
+
+     if not flashcards:
+          print("There are no flashcards available :(")
+          return
+     
+     score = 0
+     for word, answer in flashcards.items():
+          user_answer = input(f"What is the answer tp the '{word}'?")
+          if user_answer == answer:
+               score+=1
+               print(f"Correct, your score is {score}/{len(flashcards)}!")
+          else:
+               print(f"Wrong, the correct answer is: {word}")
+
+     print(f"Your final score is {score}/{len(flashcards)}")
+  
+if __name__ == "__main__":
+     main()
