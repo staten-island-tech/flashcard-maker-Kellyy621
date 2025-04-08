16ba7524cc09726ffc281bf329564c4e80a6a5f7
Kellyy621 <yekelly621@gmail.com>
2025-04-08 10:13:48 -0400

    iwhsb

--- flashcard.py
+++ flashcard.py
@@ -1,25 +1,30 @@
 import json
 
 def load_flashcards():
-    try:
+     try:
         with open('Flashcards.json', 'r') as file:
             return json.load(file)
-    except FileNotFoundError:
+     except FileNotFoundError:
         return {}
     
 def save_flashcards(flashcards):
-     with open('Flashcards', 'w') as file:
+     with open('Flashcards.json', 'w') as file:
          json.dump(flashcards, file)        
 
+flashcards = {
+     "An apple a day keeps the doctor away":"Famous saying",
+     "Mean girls":"Highschool movie"
+}
+
 def main():
-      while True:
-           mode = input("Pick your mode (Teacher/student) or 'exit' to quit")
-           if mode == 'teacher':
-                teacher_mode()
-           elif mode == 'student':
-                student_mode()
-           elif mode == 'exit':
-                break
+     while True:
+          mode = input("Pick your mode (Teacher/student) or 'exit' to quit")
+          if mode == 'teacher':
+               teacher_mode()
+          elif mode == 'student':
+               student_mode()
+          elif mode == 'exit':
+               break
 
 def teacher_mode():
     flashcards = load_flashcards()
@@ -27,7 +32,7 @@ def teacher_mode():
         word =  input ("Enter word/phrase(or'exit' to stop):")
         if word == 'exit':
                 break 
-        answer = input("Enter the answer for '{word/phrase}':")
+        answer = input("Enter the answer for '{word}':")
         flashcards[word]= answer 
     save_flashcards(flashcards)
     print("Your flashcards are saved! :)")
@@ -45,7 +50,7 @@ def student_mode():
           user_answer = input(f"What is the answer tp the '{word}'?")
           if user_answer == answer:
                score+=1
-               print(f"Correct, your score is {score}/{len(flashcards)}!")
+               print(f"Correct, your score is {score}/{len(flashcards)}!") #len counts the number of flashcards 
           else:
                print(f"Wrong, the correct answer is: {word}")
 
